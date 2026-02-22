84c12986724bfd13273706914a26aa14d65ab16d
codechef7 <patelpriyansh770@gmail.com>
2026-02-22 23:25:45 +0530

    task added

---
+++ ClassObjectsOOPS.py
@@ -0,0 +1,43 @@
+
+# class students:
+
+#     #parameterized constructor
+#     def __init__(self,name,marks)
+#         self.name=name
+#         self.marks=marks
+#         print("adding students to the database..")
+
+# s1= students("Priyansh",97)
+# print(s1.name,s1.marks)
+
+# s2= students("Aryan",90)
+# print(s2.name,s2.marks)
+
+# s3= students("Aman",80)
+# print(s3.name,s3.marks)
+
+# s4= students("Harsh",89)
+# print(s4.name,s4.marks)
+
+# s5= students("Aditya",70)
+# print(s5.name,s5.marks)
+
+
+
+#Methods
+
+# class students:
+
+#     def __init__(self,name,marks)
+#         self.name=name
+#         self.marks=marks
+    
+#     def welcome(self):
+#         print("welcome student",self.name)
+
+#     def get_marks(self):
+#         return self.marks
+
+# s1=students("Priyansh Patel",90)
+# s1.welcome()
+# print(s1.get_marks)
