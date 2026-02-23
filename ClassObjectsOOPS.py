ef01babbb4d7afd238a0d199fc9a3b089d6ea319
codechef7 <patelpriyansh770@gmail.com>
2026-02-23 12:17:41 +0530

    task added

--- ClassObjectsOOPS.py
+++ ClassObjectsOOPS.py
@@ -52,8 +52,8 @@ class students:
     def get_avg(self):
         sum=0
         for val in self.marks:
-            sum=+val
-        print("hi",self.name,"your avg score is:" sum/3)
+            sum += val
+        print("hi",self.name,'your avg score is:', sum/3)
 
 s1=students("tony stark", [99,98,97])
 s1.get_avg()
