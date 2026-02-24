a8db3e41be8fa6fc905619506e2116813a48c3a1
codechef7 <patelpriyansh770@gmail.com>
2026-02-24 22:42:33 +0530

    task added

---
+++ inheritance.py
@@ -0,0 +1,43 @@
+# #when one class (child/derived class) derives the properties and methods of another class(parents /base )
+
+# class car:
+#     @staticmethod
+#     def start():
+#         print("car started")
+
+#     @staticmethod
+#     def stop():
+#         print("car stopped")
+
+# class ToyotaCar(car):
+#     def __init__(self, name):
+#         self.name=name
+# car1= ToyotaCar("fortuner")
+# car2=ToyotaCar("prius")
+
+# print(car1.name)
+# print(car2.name)
+
+
+
+# class car:
+#     @staticmethod
+#     def start():
+#         print("car started")
+
+#     @staticmethod
+#     def stop():
+#         print("car stopped")
+
+# class ToyotaCar(car):
+#     def __init__(self, name):
+#         self.name=name
+# car1= ToyotaCar("fortuner")
+# car2=ToyotaCar("prius")
+
+# print(car1.start())
+# print(car2.start())
+
+# print(car1.stop())
+# print(car2.stop())
+
